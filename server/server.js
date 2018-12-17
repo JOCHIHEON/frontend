4b1300753869504e2608d03989950a9812360c4e
Runtym <redfilaruntime@gmail.com>
2018-12-17 09:33:40 +0900

    server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,37 @@
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/rbd.javajs.net/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/rbd.javajs.net/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/rbd.javajs.net/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/../build'));
 
 
-app.get('/.well-known/acme-challenge/DPJnipsvh_GDIZ3OreoZlrEhiG6JC-lj3Qi-OSZdUZw',(req,res)=>{
-    res.send('DPJnipsvh_GDIZ3OreoZlrEhiG6JC-lj3Qi-OSZdUZw.x6h1i378iq8HUW7op0fg_6hqXFzYofAwp0Mbn-i2oYA');
-})
-// app.get('/*', (req, res) => {                       
-//     res.sendFile(path.resolve(__dirname,'/git/frontend/build', 'index.html'));                               
-// });
+// app.get('/.well-known/acme-challenge/DPJnipsvh_GDIZ3OreoZlrEhiG6JC-lj3Qi-OSZdUZw',(req,res)=>{
+//     res.send('DPJnipsvh_GDIZ3OreoZlrEhiG6JC-lj3Qi-OSZdUZw.x6h1i378iq8HUW7op0fg_6hqXFzYofAwp0Mbn-i2oYA');
+// })
 
-app.listen(80,function(){
-    console.log('start express server (port:80)');
-})
+app.get('/*', (req, res) => {                       
+    res.sendFile(path.resolve(__dirname,'/git/frontend/build', 'index.html'));                               
+});
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
