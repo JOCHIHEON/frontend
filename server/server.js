62964843b94ea21d42a8beb8d4b663a049da713a
Runtym <redfilaruntime@gmail.com>
2018-12-17 09:25:57 +0900

    server

--- server/server.js
+++ server/server.js
@@ -4,16 +4,12 @@ const app = express();
 app.use(express.static(__dirname + '/../build'));
 
 
-
-function setHtml(req,res,next) {
-    console.log(req.method);
-    res.sendFile(path.join(__dirname,'index.html'));
-    next();
-}
-
-app.get('/*', (req, res) => {                       
-    res.sendFile(path.resolve(__dirname,'/git/frontend/build', 'index.html'));                               
-});
+app.get('/.well-known/acme-challenge/DPJnipsvh_GDIZ3OreoZlrEhiG6JC-lj3Qi-OSZdUZw',(req,res)=>{
+    res.send('DPJnipsvh_GDIZ3OreoZlrEhiG6JC-lj3Qi-OSZdUZw.x6h1i378iq8HUW7op0fg_6hqXFzYofAwp0Mbn-i2oYA');
+})
+// app.get('/*', (req, res) => {                       
+//     res.sendFile(path.resolve(__dirname,'/git/frontend/build', 'index.html'));                               
+// });
 
 app.listen(80,function(){
     console.log('start express server (port:80)');
