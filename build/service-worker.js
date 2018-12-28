0dcc289010c3af54d1d1d4deeca3c1e0ac16f7ac
JOCHIHEON <zhixian030@gmail.com>
2018-12-28 19:48:27 +0900

    commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4139c752a67afaa0aceaa848448f316d.js"
+  "/precache-manifest.67777f18212e5856fce2f7b30d9827b9.js"
 );
 
 workbox.clientsClaim();
